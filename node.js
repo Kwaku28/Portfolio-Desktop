65928df16f77258fa024d7353e39164990094cb5
tsheporamantso <tshepo.ramantso@outlook.com>
2023-03-06 14:37:12 +0200

    Add query selector and for loop to remove vector and nav link when toggle

--- node.js
+++ node.js
@@ -6,3 +6,10 @@ vectors.addEventListener("click", () => {
   navLink.classList.toggle("active");
 });
 
+document.querySelectorAll("ul.links > li").forEach((n) =>
+  n.addEventListener("click", () => {
+    vectors.classList.remove("active");
+    navLink.classList.remove("active");
+  })
+);
+
